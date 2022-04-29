8a477486b8ef59be34cc505541f8ae794c298fdc
Aakash <kharabeaakash@gmail.com>
2022-04-29 21:52:33 +0530

    Initial Commit

---
+++ Twitter Clone/Twitter_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Twitter_CloneApp.swift
+//  Twitter Clone
+//
+//  Created by Aakash Kharabe on 29/04/22.
+//
+
+import SwiftUI
+
+@main
+struct Twitter_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
